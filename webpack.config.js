9f2f0e9b05d252424745dc74dbbf0d3be0247f39
Maxim Talanov <mail@maxtalanov.ru>
2024-06-12 03:15:46 +0300

    feat(webpack config): настроен файл webpack.config.js
    Добавлены настройки для точки входа и выходного файла.
    Указаны расширения файлов для разрешения.
    Настроены правила загрузки для TypeScript и CSS файлов.
    Заданы настройки devServer для локальной разработки.
    Добавлен плагин Dotenv для загрузки переменных окружения из файла .env.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: process.env.PORT || 9000
+  },
+  plugins: [
+    new Dotenv()
+  ]
+};
